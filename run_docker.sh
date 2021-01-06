22a3030d7b73c826d6d5d8397bf4bb4b9b23a513
James <jpkatarikawe@gmail.com>
2021-01-06 10:20:52 +0300

    Complete script to build image and run docker container

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
-
-# Step 1:
 # Build image and add a descriptive tag
+docker build --tag=jpkat/ml-microservice . 
 
-# Step 2: 
 # List docker images
+docker images
 
-# Step 3: 
 # Run flask app
+docker run -i -p 8000:80 -t jpkat/ml-microservice
