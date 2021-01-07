8068540eab846d9d17b74b09c91285a6730ecc12
James <jpkatarikawe@gmail.com>
2021-01-07 12:52:56 +0300

    Complete kubernetes shell script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=jpkat/ml-microservice
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-service --image=jpkat/ml-microservice:latest --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-service 8000:80
