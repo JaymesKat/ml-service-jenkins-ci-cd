31f1a8bdea4143b42acb7cf21a30963dffc48a93
James <jpkatarikawe@gmail.com>
2021-03-10 15:09:44 +0300

    use variable for region when uploading docker image

--- scripts/build_upload_docker.sh
+++ scripts/build_upload_docker.sh
@@ -5,7 +5,7 @@
 
 # ECR host
 ecr_host=417548009410.dkr.ecr.us-west-1.amazonaws.com
-
+region=us-west-1
 # Step 1:
 # Create docker image
 docker_image=ml-microservice
@@ -13,7 +13,7 @@ docker build --tag="$docker_image" .
 
 # Step 2:  
 # Authenticate with Amazon ECR
-aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin "$ecr_host"
+aws ecr get-login-password --region "$region" | docker login --username AWS --password-stdin "$ecr_host"
 
 # Step 3:
 # Tag and push image to a ECR repository
